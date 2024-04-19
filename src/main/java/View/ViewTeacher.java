3c144f73d5df5b24fd2594a8b753cd176e6c241b
Duccum123 <166524580+Duccum123@users.noreply.github.com>
2024-04-19 19:30:26 +0700

    hehee

--- src/main/java/View/ViewTeacher.java
+++ src/main/java/View/ViewTeacher.java
@@ -3,30 +3,35 @@ package View;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JDesktopPane;
-import java.awt.BorderLayout;
 import javax.swing.JPanel;
-import javax.swing.BoxLayout;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import javax.swing.JButton;
-import java.awt.Font;
-import javax.swing.JList;
-import javax.swing.AbstractListModel;
-import javax.swing.JTextPane;
-import javax.swing.JSeparator;
 import javax.swing.JScrollPane;
-import javax.swing.JSpinner;
-import javax.swing.JProgressBar;
-import javax.swing.JToggleButton;
-import javax.swing.JFormattedTextField;
-import javax.swing.JPasswordField;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
+import javax.swing.JSplitPane;
+import javax.swing.JInternalFrame;
+import java.awt.Color;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.UIManager;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
 
-public class ViewTeacher {
+public class ViewTeacher extends JFrame {
 
-	private JFrame frame;
-	private JPasswordField passwordField;
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -35,8 +40,8 @@ public class ViewTeacher {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ViewTeacher window = new ViewTeacher();
-					window.frame.setVisible(true);
+					ViewTeacher frame = new ViewTeacher();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -45,100 +50,214 @@ public class ViewTeacher {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public ViewTeacher() {
-		initialize();
-	}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 700);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1058, 598);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		PanelRound panel = new PanelRound(20);
-		panel.setBounds(0, 0, 1042, 61);
-		frame.getContentPane().add(panel);
-		
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(50, 185, 185));
+		panel.setBounds(0, 0, 170, 661);
+		contentPane.add(panel);
 		panel.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Trường THPT Vinh");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel.setBounds(10, 11, 240, 35);
-		panel.add(lblNewLabel);
-		
-		JButton btnNewButton = new JButton("Exam");
-		btnNewButton.setBounds(339, 21, 89, 23);
+
+		MyButton btnNewButton = new MyButton("Home");
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton.setBounds(10, 90, 137, 37);
+		btnNewButton.setBackground(new Color(50, 185, 185));
+		btnNewButton.setColorClick(new Color(50, 185, 185));
+		btnNewButton.setColorOver(new Color(100, 241, 241));
+		btnNewButton.setRadius(10);
+		btnNewButton.setBorderColor(new Color(50, 185, 185));
+		btnNewButton.setIcon(new ImageIcon(
+				Toolkit.getDefaultToolkit().createImage(getClass().getResource("/view/image/icons8-home-20.png"))));
 		panel.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Question Library");
-		btnNewButton_1.setBounds(444, 21, 124, 23);
-		panel.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Cài Đặt");
-		btnNewButton_2.setBounds(759, 18, 68, 29);
-		panel.add(btnNewButton_2);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(849, 25, 46, 14);
-		panel.add(lblNewLabel_1);
-		
-		JButton btnNewButton_4 = new JButton("Lớp học");
-		btnNewButton_4.setBounds(594, 21, 89, 23);
-		panel.add(btnNewButton_4);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 86, 1042, 61);
-		frame.getContentPane().add(panel_1);
+
+		MyButton btnNewButton1 = new MyButton("Classes");
+		btnNewButton1.setForeground(new Color(255, 255, 255));
+		btnNewButton1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton1.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton1.setBounds(10, 138, 137, 37);
+		btnNewButton1.setBackground(new Color(50, 185, 185));
+		btnNewButton1.setColorClick(new Color(50, 185, 185));
+		btnNewButton1.setColorOver(new Color(100, 241, 241));
+		btnNewButton1.setRadius(10);
+		btnNewButton1.setBorderColor(new Color(50, 185, 185));
+		btnNewButton1.setIcon(new ImageIcon(
+				Toolkit.getDefaultToolkit().createImage(getClass().getResource("/view/image/icons8-class-20.png"))));
+		panel.add(btnNewButton1);
+
+		MyButton btnNewButton2 = new MyButton("Results");
+		btnNewButton2.setForeground(new Color(255, 255, 255));
+		btnNewButton2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton2.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton2.setBounds(10, 186, 137, 37);
+		btnNewButton2.setBackground(new Color(50, 185, 185));
+		btnNewButton2.setColorClick(new Color(50, 185, 185));
+		btnNewButton2.setColorOver(new Color(100, 241, 241));
+		btnNewButton2.setRadius(10);
+		btnNewButton2.setBorderColor(new Color(50, 185, 185));
+		btnNewButton2.setIcon(new ImageIcon(
+				Toolkit.getDefaultToolkit().createImage(getClass().getResource("/view/image/icons8-result-20.png"))));
+		panel.add(btnNewButton2);
+
+		MyButton btnNewButton3 = new MyButton("Profile");
+		btnNewButton3.setForeground(new Color(255, 255, 255));
+		btnNewButton3.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton3.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton3.setBounds(10, 234, 137, 37);
+		btnNewButton3.setBackground(new Color(50, 185, 185));
+		btnNewButton3.setColorClick(new Color(50, 185, 185));
+		btnNewButton3.setColorOver(new Color(100, 241, 241));
+		btnNewButton3.setRadius(10);
+		btnNewButton3.setBorderColor(new Color(50, 185, 185));
+		btnNewButton3.setIcon(new ImageIcon(Toolkit.getDefaultToolkit()
+				.createImage(getClass().getResource("/view/image/icons8-male-user-20.png"))));
+		panel.add(btnNewButton3);
+
+		MyButton btnNewButton4 = new MyButton("Create new");
+		btnNewButton4.setForeground(new Color(255, 255, 255));
+		btnNewButton4.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton4.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton4.setBounds(10, 282, 137, 37);
+		btnNewButton4.setBackground(new Color(50, 185, 185));
+		btnNewButton4.setColorClick(new Color(50, 185, 185));
+		btnNewButton4.setColorOver(new Color(100, 241, 241));
+		btnNewButton4.setRadius(10);
+		btnNewButton4.setBorderColor(new Color(50, 185, 185));
+		btnNewButton4.setIcon(new ImageIcon(
+				Toolkit.getDefaultToolkit().createImage(getClass().getResource("/view/image/icons8-add-new-20.png"))));
+		panel.add(btnNewButton4);
+
+		PanelRound panel_1 = new PanelRound(15);
+		panel_1.setBackground(new Color(221, 221, 221));
+		panel_1.setBorderColor(getBackground());
+		panel_1.setBounds(178, 80, 440, 280);
+		panel_1.setBground(getBackground());
+		contentPane.add(panel_1);
 		panel_1.setLayout(null);
+
+		PanelRound panel_2 = new PanelRound(15);
+		panel_2.setBackground(new Color(221, 221, 221));
+		panel_2.setBounds(625, 95, 255, 255);
+		panel_2.setBorderColor(getBackground());
+		panel_2.setBground(getBackground());
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+
+		JLabel lblNewLabel_1 = new JLabel("Create");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1.setBounds(10, 11, 55, 14);
+		panel_2.add(lblNewLabel_1);
+
+		MyButton btnNewButton_1 = new MyButton("Create test");
+		btnNewButton_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton_1.setBounds(37, 200, 181, 40);
+		btnNewButton_1.setBackground(new Color(50, 185, 185));
+		btnNewButton_1.setRadius(10);
+		btnNewButton_1.setColor(new Color(50, 185, 185));
+		btnNewButton_1.setBorderColor(new Color(240, 240, 240));
+		btnNewButton_1.setColorOver(new Color(100, 241, 241));
+		btnNewButton_1.setColorClick(new Color(50, 185, 185));
+		panel_2.add(btnNewButton_1);
+
+		JLabel lblNewLabel_3 = new JLabel();
+		lblNewLabel_3.setBounds(10, 36, 235, 153);
+		lblNewLabel_3.setIcon(new ImageIcon(Toolkit.getDefaultToolkit()
+				.createImage(getClass().getResource("/view/image/study.jpg"))
+				.getScaledInstance(lblNewLabel_3.getWidth(), lblNewLabel_3.getHeight(), Image.SCALE_SMOOTH)));
 		
-		JButton btnNewButton_3 = new JButton("Thêm bài kiểm tra");
-		btnNewButton_3.setBounds(680, 11, 123, 23);
-		panel_1.add(btnNewButton_3);
+		panel_2.add(lblNewLabel_3);
+
+		JLabel lblNewLabel = new JLabel("Xin chào Đức đẹp trai");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel.setBounds(180, 0, 160, 24);
+		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setBounds(25, 0, 46, 14);
-		panel_1.add(lblNewLabel_2);
+		PanelRound panel_2_1 = new PanelRound(15);
+		panel_2_1.setLayout(null);
+		panel_2_1.setBorderColor(UIManager.getColor("Button.background"));
+		panel_2_1.setBground(UIManager.getColor("Button.background"));
+		panel_2_1.setBackground(new Color(221, 221, 221));
+		panel_2_1.setBounds(625, 371, 255, 255);
+		contentPane.add(panel_2_1);
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		lblNewLabel_3.setBounds(81, 0, 46, 14);
-		panel_1.add(lblNewLabel_3);
+		JLabel lblNewLabel_1_1 = new JLabel("Schedules test");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1_1.setBounds(10, 11, 116, 14);
+		panel_2_1.add(lblNewLabel_1_1);
 		
-		JList list = new JList();
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Lop a  lop c"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		list.setBounds(10, 173, 1032, 48);
+		MyButton btnNewButton_1_1 = new MyButton("Create test");
+		btnNewButton_1_1.setText("Set another");
+		btnNewButton_1_1.setRadius(10);
+		btnNewButton_1_1.setForeground(Color.WHITE);
+		btnNewButton_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton_1_1.setColorOver(new Color(100, 241, 241));
+		btnNewButton_1_1.setColorClick(new Color(50, 185, 185));
+		btnNewButton_1_1.setColor(new Color(50, 185, 185));
+		btnNewButton_1_1.setBorderColor(UIManager.getColor("Button.background"));
+		btnNewButton_1_1.setBackground(new Color(50, 185, 185));
+		btnNewButton_1_1.setBounds(37, 200, 181, 40);
+		panel_2_1.add(btnNewButton_1_1);
 		
-		JScrollPane scrollPane = new JScrollPane(list);
-		scrollPane.setBounds(0, 165, 1032, 241);
-		frame.getContentPane().add(scrollPane);
+		JLabel lblNewLabel_3_1 = new JLabel();
+		lblNewLabel_3_1.setBounds(10, 36, 235, 153);
+		/*
+		 * lblNewLabel_3_1.setIcon(new ImageIcon(Toolkit.getDefaultToolkit()
+		 * .createImage(getClass().getResource("/view/image/schedule.jpg"))
+		 * .getScaledInstance(lblNewLabel_3_1.getWidth(), lblNewLabel_3_1.getHeight(),
+		 * Image.SCALE_SMOOTH)));
+		 */
+		panel_2_1.add(lblNewLabel_3_1);
 		
-		JToggleButton tglbtnNewToggleButton = new JToggleButton("New toggle button");
-		tglbtnNewToggleButton.setBounds(32, 437, 121, 23);
-		frame.getContentPane().add(tglbtnNewToggleButton);
+		JLabel lblNewLabel_2 = new JLabel("Edit");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(199, 13, 46, 14);
+		panel_2_1.add(lblNewLabel_2);
 		
-		JFormattedTextField formattedTextField = new JFormattedTextField();
-		formattedTextField.setBounds(243, 438, 187, 23);
-		frame.getContentPane().add(formattedTextField);
+		PanelRound panel_1_1 = new PanelRound(15);
+		panel_1_1.setLayout(null);
+		panel_1_1.setBorderColor(UIManager.getColor("Button.background"));
+		panel_1_1.setBground(UIManager.getColor("Button.background"));
+		panel_1_1.setBackground(new Color(221, 221, 221));
+		panel_1_1.setBounds(178, 360, 440, 280);
+		contentPane.add(panel_1_1);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(514, 438, 274, 22);
-		frame.getContentPane().add(passwordField);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"sds", "s", "s", "s", "s", "s", "", "s"}));
-		comboBox.setBounds(821, 437, 211, 53);
-		frame.getContentPane().add(comboBox);
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("C Grade", 15);
+        dataset.setValue("B Grade", 66);
+        dataset.setValue("Failing", 9);
+        dataset.setValue("A Grade", 10);
+    	
+        JFreeChart chart = ChartFactory.createPieChart(
+                "Recent test results", dataset, true, true, true);
+        PiePlot pie = (PiePlot) chart.getPlot();
+        pie.setSectionPaint("B Grade",new Color(50, 185, 185));
+        pie.setSectionPaint("A Grade",Color.red);
+        pie.setSectionPaint("C Grade",Color.green);
+        pie.setSectionPaint("Failing",Color.yellow);
+        pie.setBackgroundPaint(Color.WHITE);
+    	
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(null);
+        chartPanel.setForeground(new Color(255, 255, 255));
+        chartPanel.setBounds(10, 10, 420, 260);
+        chartPanel.setBackground(new Color(255, 255, 255));
+        
+        panel_1.add(chartPanel);
+        
+        
 	}
 }
