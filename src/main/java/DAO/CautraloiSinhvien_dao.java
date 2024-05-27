425874fd04261d3c35b280f1ac8394e16f9c21f8
Duccum123 <166524580+Duccum123@users.noreply.github.com>
2024-05-27 10:08:31 +0700

    Update CautraloiSinhvien_dao.java

--- src/main/java/DAO/CautraloiSinhvien_dao.java
+++ src/main/java/DAO/CautraloiSinhvien_dao.java
@@ -65,6 +65,76 @@ public class CautraloiSinhvien_dao  {
 			}
 			return result;
 	}
+	public List<Cautraloisinhvien> selectByIdCauhoi(String idcauhoi)
+	{
+		List<Cautraloisinhvien> result = new ArrayList<Cautraloisinhvien>();
+		try {
+			Connection con  = JDBCUtil.getConnection();
+			String sql = " select * from cautraloisinhvien where cauhoi_id = ? ";
+			
+			PreparedStatement a;
+
+				a = con.prepareStatement(sql);
+				a.setString(1, idcauhoi);
+				ResultSet kq = a.executeQuery();
+				
+				while(kq.next())
+				{
+					String id = kq.getString("id");
+					String cauhoi= kq.getString("cauhoi_id");
+					String cautraloi = kq.getString("cautraloi");
+					String bailamid = kq.getString("bailam");
+					Cautraloisinhvien u = new Cautraloisinhvien();
+					BaiLam b = BaiLam_dao.Instance().selectbyid(bailamid);
+					u.setBailamsv(b);
+					Cauhoi ch = new Cauhoi();
+					ch.setId(cauhoi);
+					u.setCauhoi(Cauhoi_Dao.Instance().selectbyid(ch));
+					u.setCautraloi(cautraloi);
+					u.setId(id);
+					result.add(u);
+				}
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return result;
+	}
+	public Cautraloisinhvien selectByid(String idctrl)
+	{
+		try {
+			Connection con  = JDBCUtil.getConnection();
+			String sql = " select * from cautraloisinhvien where id = ? ";
+			
+			PreparedStatement a;
+
+				a = con.prepareStatement(sql);
+				a.setString(1, idctrl);
+				ResultSet kq = a.executeQuery();
+				
+				while(kq.next())
+				{
+					String id = kq.getString("id");
+					String cauhoi= kq.getString("cauhoi_id");
+					String cautraloi = kq.getString("cautraloi");
+					String bailamid = kq.getString("bailam");
+					Cautraloisinhvien u = new Cautraloisinhvien();
+					Cauhoi ch = new Cauhoi();
+					ch.setId(cauhoi);
+					BaiLam b = BaiLam_dao.Instance().selectbyid(bailamid);
+					u.setBailamsv(b);
+					u.setCauhoi(Cauhoi_Dao.Instance().selectbyid(ch));
+					u.setCautraloi(cautraloi);
+					u.setId(id);
+				}
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+	}
 	public boolean deletebyid(Cautraloisinhvien t) {
 		SessionFactory sessionFactory = HibernateUtil.getSessionfacFactory();
 		if(sessionFactory!=null)
